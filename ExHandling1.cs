8980b9d1dd3acff7a4d23c4b7844ea4173bf8c23
buituananh12345 <103102389+buituananh12345@users.noreply.github.com>
2022-07-25 17:17:57 +0700

    Add files via upload

---
+++ ExHandling1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bai05
+{
+    class Vidu1
+    {
+        static void Main(string[] args)
+        {
+            byte[] a = new byte[5];
+
+            //nhap mang
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("a[{0}]=" i + 1);
+                    a[i] = Convert.ToByte(Console.ReadLine());
+                }
+            }
+            catch (FormatException ex)
+            {
+                //Console.WritenLine(ex.Message);
+                Console.WriteLine("Khong duoc nhap ki tu cho mang so");
+            }
+            catch (OverflowException ex)
+            {
+                //Console.WritrnLine(ex.Message);
+                Console.WriteLine("Khong duoc nhap gia tri nam ngoai mien 0-255");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                //Console.WritenLine(ex.Message);
+                Console.WritenLine("Loi vuot qua pham vi cua mang");
+            }
+
+            //in mang
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(" {0}", a[i]);
+        }
+    }
+}
